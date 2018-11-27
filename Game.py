8b7ccbf4eeac0e859788ce6c61bc9ce56f9e9aa4
Rich Hildred <rhildred@ip-172-31-5-173.us-west-2.compute.internal>
2018-11-27 12:47:26 -0500

    made the start of a choose your own adventure

--- Game.py
+++ Game.py
@@ -1,39 +1,40 @@
-import random
+class Welcoming:
+    def __init__(self, outer):
+        self.__outer = outer
+    def takeTurn(self, sPhone, sInput):
+        self.__outer.setState("stick")
+        return ["Hi I'm spot (a dog). Oh look a stick. Do you play or keep on walking?"]
+
+class Stick:
+    def __init__(self, outer):
+        self.__outer = outer
+    def takeTurn(self, sPhone, sInput):
+        if "play" in sInput.lower():
+            self.__outer.setState("play")
+            return ["Great my favourite game ... Here's the stick back. Do you throw it again?"]
+        else:
+            return ["Walking is my favourite. Oh look a stick! Do you play or keep on walking?"]
+
+class Play:
+    def __init__(self, outer):
+        self.__outer = outer
+    def takeTurn(self, sPhone, sInput):
+        if "yes" in sInput.lower():
+            return ["Here it is! I got it for you. Do you toss it again.... Please? "]
+        else:
+            self.__outer.setState("stick")
+            return ["Walking is my favourite. Oh look a stick! Do you play or keep on walking?"]
 
 class Game:
     def __init__(self):
-        self.__nCurrent = -1
+        self.oStates = {
+            "welcoming": Welcoming(self),
+            "stick": Stick(self),
+            "play": Play(self)
+        }
+        self.__sCurState = "welcoming"
+    def setState(self, sNew):
+        self.__sCurState = sNew
     def takeTurn(self, sPhone, sInput):
-        if(self.__nCurrent == -1):
-            self.__nCurrent = 0
-            return["Welcome to 20 + 1",
-                   "in this game the object is to make the computer count 21",
-                   "you count from 1-4 steps on the way to 21"
-                ]
-        else:
-            aNumbers = sInput.split(",")
-            for n in aNumbers:
-                self.__nCurrent += 1
-            if self.__nCurrent > 15:
-                if self.__nCurrent == 20:
-                    self.__nCurrent = -1
-                    return["21", "you win"]
-                else:
-                    sNumbers = ""
-                    while self.__nCurrent < 20:
-                        if sNumbers != "":
-                            sNumbers += ", "
-                        self.__nCurrent += 1
-                        sNumbers += str(self.__nCurrent)
-                    self.__nCurrent = -1
-                    return[sNumbers, "I win ... ha"]
-            else:
-                nCount = random.randint(1,4)
-                sNumbers = ""
-                for n in range(0, nCount):
-                    if sNumbers != "":
-                        sNumbers += ", "
-                    self.__nCurrent += 1
-                    sNumbers += str(self.__nCurrent)
-                return[sNumbers]
-                
+        return self.oStates[self.__sCurState].takeTurn(sPhone, sInput)
+
